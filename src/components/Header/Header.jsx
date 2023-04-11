6ae4ebbdbdf2f1e04eff91339b506a0fc6777b06
Humayun1318 <humayunKabir6267@gmail.com>
2023-04-12 00:28:48 +0600

    category load

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,18 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='Header'>
-            <h2 className='header-logo'> Career Hub</h2>
-            <nav >
-                <Link className='nav' to='/'>Home</Link>
-                <Link className='nav' to='/Statistics'>Statistics</Link>
-                <Link className='nav' to='/AppliedJobs'>Applied Jobs</Link>
-                <Link className='nav' to='/Blog'>Blog</Link>
-            </nav>
-            <div className='Header-btn-d'><button className='Header-btn'>Star Applying</button></div>
-        </div>
+        <div className='header-clr'>
+            <div className='Header'>
+                <h2 className='header-logo'> Career Hub</h2>
+                <nav >
+                    <Link className='nav' to='/'>Home</Link>
+                    <Link className='nav' to='/Statistics'>Statistics</Link>
+                    <Link className='nav' to='/AppliedJobs'>Applied Jobs</Link>
+                    <Link className='nav' to='/Blog'>Blog</Link>
+                </nav>
+                <div className='Header-btn-d'><button className='Header-btn'>Star Applying</button></div>
+            </div>
+       </div>
         
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
+import Category from '../Category/Category';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div className='banner-section'>
-            <div className='banner-title'>
-                <h1>One Step Closer To Your <span className='dream'>Dream Job</span></h1>
-                <p className='banner-des'>Explore thousands of job opportunities with all the information 
-                 you need. Its your future. Come find it. Manage all your job application from start to 
-                 finish.</p>
-                <button className='banner-btn'>Get Started</button>
-            </div>
-            <div className='banner-img'>
-                {/* <img src="../../../src/assets/All Images/bannerPerson.png" alt="" /> */}
+    const categoryData = useLoaderData()
+    // console.log(categoryData);
+    const [features, setFeatures] = useState([])
 
+    useEffect(() => {
+        fetch('jobDb.json')
+            .then(res => res.json())
+            .then(data => setFeatures(data))
+    }, [])
+
+    return (
+        <div>
+            <div className='banner-clr'>
+                <div className='banner-section'>
+                    <div className='banner-title'>
+                        <h1>One Step Closer To Your <br /><span className='dream'>Dream Job</span></h1>
+                        <p className='banner-des'>Explore thousands of job opportunities with all the
+                            information
+                            you need. Its your future. Come find it. Manage all your job application from
+                            start to
+                            finish.</p>
+                        <button className='banner-btn'>Get Started</button>
+                    </div>
+                    <div className='banner-img'>
+                        <img src="../../../src/assets/All Images/bannerPerson.png" alt="" />
+
+                    </div>
+                </div>
+            </div>
+            {/* job category */}
+            <div className='ctg-container'>
+                <h1 className='ctg-title'>Job Category List</h1>
+                <p className='ctg-des'>Explore thousands of job opportunities with all the information you
+                    need. Its your future</p>
+                <div className=' ' >
+
+                    {
+                        categoryData.map(category => <Category
+                            key={category.id}
+                            category={category}>
+                            
+                            </Category>
+                            
+                       )
+                    }
+                </div>
             </div>
         </div>
     );
